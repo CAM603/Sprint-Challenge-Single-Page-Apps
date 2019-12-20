2bbc8b5b5b66cd1b485f7968213ae05ea2fe19ef
CAM603 <cameronhawley@comcast.net>
2019-12-20 12:47:02 -0500

    added reactstrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Navbar, NavItem, Nav, NavbarText } from 'reactstrap';
+
 export default function Navigation() {
     return (
-        <nav>
-            <Link to="/">
-                <p>Home</p>
-            </Link>
-            <Link to="/characters">
-                <p>Characters</p>
-            </Link>
-            <Link to="/locations">
-                <p>Locations</p>
-            </Link>
-            <Link to="/episodes">
-                <p>Episodes</p>
-            </Link>
-        </nav>
+        <Navbar>
+            <Nav>
+                <NavItem>
+                    <Link to="/">
+                        <NavbarText>Home</NavbarText>
+                    </Link> 
+                </NavItem>
+            </Nav>
+            <Nav>
+                <NavItem>
+                    <Link to="/characters">
+                    <NavbarText>Characters</NavbarText>
+                    </Link>
+                </NavItem>
+            </Nav>
+            <Nav>
+                <NavItem>
+                    <Link to="/locations">
+                    <NavbarText>Locations</NavbarText>
+                    </Link>
+                </NavItem>
+            </Nav>
+            <Nav>
+                <NavItem>
+                    <Link to="/episodes">
+                    <NavbarText>Episodes</NavbarText>
+                    </Link>
+                </NavItem>
+            </Nav>
+        </Navbar>
     )
 }
