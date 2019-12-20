3fc243dd401f75a9a6375be6bddd6d72f6d6755a
CAM603 <cameronhawley@comcast.net>
2019-12-20 11:33:36 -0500

    getting characters

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  console.log(character)
+  return (
+    <div>
+      <h1>{character.name}</h1>
+      <img src={character.image} />
+      <h2>Status: {character.status}</h2>
+      <h2>Species: {character.species}</h2>
+    </div>
+  )
 }
