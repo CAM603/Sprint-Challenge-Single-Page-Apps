1b332981d469ab588e9749a53d7816352230ac03
CAM603 <cameronhawley@comcast.net>
2019-12-22 09:49:15 -0500

    getting character page

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,10 +3,48 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 export default function Character() {
+    
+    // Set state for loading
+    const [loading, setLoading] = useState(true);
+    const [character, setCharacter] = useState([]);
+    const [origin, setOrigin] = useState([]);
+    const [location, setLocation] = useState([]);
 
-    console.log(useParams())
+    const {id} = useParams();
+
+    useEffect(() => {
+        setLoading(true)
+        let cancel
+    
+        axios
+        .get(`https://rickandmortyapi.com/api/character/${id}`, {
+            cancelToken: new axios.CancelToken(c => cancel = c)
+        })
+        .then(response => {
+            console.log(response.data);
+            setLoading(false)
+            setCharacter(response.data);
+            setOrigin(response.data.origin);
+            setLocation(response.data.location)
+            
+        })
+        .catch(error => console.log(error))
+        // Cleanup function
+        return () => cancel()
+    
+    }, [id]);
+    
+        if (loading) return 'Loading...'
+
     return (
-
-        <h1>Character</h1>
+        <div>
+            <h1>{character.name}</h1>
+            <h3>{character.status}</h3>
+            <h3>{character.species}</h3>
+            <h3>{character.type}</h3>
+            <h3>{character.gender}</h3>
+            <h3>{origin.name}</h3>
+            <h3>{location.name}</h3>
+        </div>
     )
 }
